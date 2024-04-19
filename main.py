26fa141e105d013dea4d689cd1119a2d683cfd30
Charlxy <charly59000@orange.fr>
2024-04-19 12:07:39 +0200

    modif render templates

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/accueil')
 def start():
-    return render_template("./page1.html")
+    return render_template("./flaskr/page1.html")
 
 @app.route('/learn-more')
 def page2():
-    return render_template("./page2.html")
+    return render_template("./flaskr/page2.html")
 
 @app.route('/product-Home')
 def  product_home():
-    return render_template('./product-page.html') 
+    return render_template('./flaskr/product-page.html') 
 
 @app.route('/login')
 def login():
-    return render_template('./LogIn.html')
+    return render_template('./flaskr/LogIn.html')
 
 @app.route('/register')
 def register():
-    return render_template('./register.html')
+    return render_template('./flaskr/register.html')
 
 @app.route("/404")
 def page_404():
-    return render_template('./404.html')
+    return render_template('./flaskr/404.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
