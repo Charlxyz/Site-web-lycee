44da50765f96036233f6e5c0f1597ce238775b2c
Musubi42 <raphaelthi59@gmail.com>
2024-04-19 12:47:47 +0200

    feat(Dockerization): docekrizé application flask
    modification du chemin template

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM python:3.10
+# Use an official Python runtime as a parent image
+FROM python:3.8-slim
 
+# Set the working directory in the container
 WORKDIR /app
 
-COPY ./requirements.txt .
+# Copy the current directory contents into the container at /app
+COPY . /app
 
-RUN pip install -r requirements.txt
+EXPOSE 8001
 
-COPY . .
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
+# Run app.py when the container launches
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,34 +1,37 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./flaskr/templates', static_folder='./flaskr/static')
 
 @app.route('/')
 @app.route('/accueil')
 def start():
-    return render_template("./flaskr/page1.html")
+    return render_template("page1.html")
 
 @app.route('/learn-more')
 def page2():
-    return render_template("./flaskr/page2.html")
+    return render_template("page2.html")
 
 @app.route('/product-Home')
 def  product_home():
-    return render_template('./flaskr/product-page.html') 
+    return render_template('product-page.html') 
 
 @app.route('/login')
 def login():
-    return render_template('./flaskr/LogIn.html')
+    return render_template('LogIn.html')
 
 @app.route('/register')
 def register():
-    return render_template('./flaskr/register.html')
+    return render_template('register.html')
 
 @app.route("/404")
 def page_404():
-    return render_template('./flaskr/404.html')
+    return render_template('404.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
     return page_404()
 
-app.run(debug=True)
+# app.run(debug=True)
+
+# if __name__ == '__main__':
+app.run(debug=True, host='0.0.0.0', port=8001)
